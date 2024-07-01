28da5304505fee7faeef041e48409a524fdba79a
Augusto Lemes <augustolemes2006@gmail.com>
2024-07-01 09:09:45 -0300

    Update Mega-Sena.py

--- Mega-Sena.py
+++ Mega-Sena.py
@@ -1,83 +1,29 @@
 from random import randint
 import os
 
-lista_de_jogos = []
-
+num = []
 while True:
     os.system('cls')
+    lista = []
 
-    # Variável para adicionar ou não para a lista
-
-    d = True
-
-    # Realizar o sorteio
-
-    n1 = randint(1, 60)
-    n2 = randint(1, 60)
-    n3 = randint(1, 60)
-    n4 = randint(1, 60)
-    n5 = randint(1, 60)
-    n6 = randint(1, 60)
+    for i in range(1, 7):    # Realizar o sorteio
+        sorteio = randint(1, 61)
+        lista.append(sorteio)
     
-    sorteios = (n1, n2, n3, n4, n5, n6)
+    a = lista     # Deletar jogos idênticos
+    b = set(lista)
+    if len(b) < len(a):
+        del lista
+        continue
 
-    # Filtro I: Filtros repetidos no sorteio
-
-    if n1 == n2:
-        d = False
-    else:
-        if n1 == n3:
-            d = False
-        else:
-            if n1 == n4:
-                d = False
-            else:
-                if n1 == n5:
-                    d = False
-                else:
-                    if n1 == n6:
-                        d = False
-                    else:
-                        if n2 == n3:
-                            d = False
-                        else:
-                            if n2 == n4:
-                                d = False
-                            else:
-                                if n2 == n5:
-                                    d = False
-                                else:
-                                    if n2 == n6:
-                                        d = False
-                                    else:
-                                        if n3 == n4:
-                                                d = False
-                                        else:
-                                            if n3 == n5:
-                                                d = False
-                                            else:
-                                                if n3 == n6:
-                                                    d = False
-                                                else:
-                                                    if n4 == n5:
-                                                        d = False
-                                                    else:
-                                                        if n4 == n6:
-                                                            d = False
-                                                        else:
-                                                            if n5 == n6:
-                                                                d = False
-    if d == True:
-        lista_de_jogos.append(sorteios)
+    lista = tuple(lista)
+    num.append(lista)
     
-    # Filtro II: Sorteios idênticos
+    num_tuplas = set(num)    # Deletar sorteios idênticos
 
-    lista_de_jogos_tupla = set(lista_de_jogos)
-    sorteios2 = list(lista_de_jogos_tupla)
-    sorteios = sorteios2
-    print(sorteios)
+    num2 = list(num_tuplas)
+    num = num2
+    print(num)
 
-    # Finalizar o script automaticamente
-
-    if len(sorteios) == 50063860:
+    if len(num) == 50063860:    # Finalizar o script automaticamente
         break
